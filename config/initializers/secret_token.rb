a3dfddabe746b651e97717a998935210a30df92b
DPL-Delta <dpl-delta@dpl-deltas-imac.domain.actdsltmp>
2014-03-04 23:39:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Handthatfeeds::Application.config.secret_token = '730d5771589bb6825250957e75fd610c199a620df73f3737786756382337c97b10133b9fe977e4072840c7960ec4c8b670fd1d201228a8436bab25a13b7d58fe'
