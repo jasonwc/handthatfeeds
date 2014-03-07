be7b0b95775363464665ae6d9e78ec460264ffef
Triprime <shelton_matthew@hotmail.com>
2014-03-06 19:29:42 -0700

    Fixes all contact_us funcitonality

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,8 @@
 class ContactMailer < ActionMailer::Base
 
-
   def submission(message)
     @message = message
 
-    # we can change the to: line when we have a project email
     mail( to: "politics@project.com", 
           subject: "#{@message.subject}", 
           from: "#{@message.name}"
