95b7d484b23bb9bcc6fd19126ba0af3f9cffa5c3
marlenycotrim <marlenycotrim@gmail.com>
2014-03-25 11:24:31 -0600

    adds user logic for session views and paperclip validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :first_name, :last_name, :avatar
-
   has_attached_file :avatar
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :first_name, :last_name, :avatar
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   #validates :username, presence: true
   #validates :username, presence: true, on: :edit
