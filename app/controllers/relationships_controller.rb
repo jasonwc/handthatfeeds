1f7f79a32cd5ebe7fe4ae274e3632aef69881320
jasonwc <jasonwcarter@outlook.com>
2014-03-27 22:03:02 -0600

    Changes home layout, adds styles and follow button to User.

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,11 @@
+# class RelationshipsController < ApplicationController
+#   def create
+#     @relationship = Relationship.new(params[:relationship])
+#     format.html { redirect_to(root_path, :notice => 'You successfully followed a legislator!')
+#   end
+
+#   def destroy
+#     @relationship = Relationship.find(params[:id])
+#     @relationship.destroy
+#   end
+# end
--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,13 @@ ActiveRecord::Schema.define(:version => 20140326162914) do
     t.string   "crp_id"
   end
 
+  create_table "relationships", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "legislator_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
     t.string   "encrypted_password",     :default => "",    :null => false
